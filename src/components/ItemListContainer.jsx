afeb8623714c1ae704d3987d081122c3a9b7e537
Juan Ignacio Murphy <juanimurphy@gmail.com>
2024-07-24 23:20:48 -0300

    Teminado para entregar

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,11 +2,14 @@ import Container from "react-bootstrap/Container";
 
 export const ItemListContainer = (props) => {
     return (
+
     <Container className="mt-4">
 
         <h2>{props.greeting}</h2>
 
     </Container>
+
+
 
     );
 }
