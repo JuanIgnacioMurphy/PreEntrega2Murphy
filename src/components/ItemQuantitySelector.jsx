2455b041f2fb70cfd62fc3e8abc4f9b2db3d9220
Juan Ignacio Murphy <juanimurphy@gmail.com>
2024-09-05 04:29:24 -0300

    Finalicé, falta pulir

--- src/components/ItemQuantitySelector.jsx
+++ src/components/ItemQuantitySelector.jsx
@@ -1,32 +1,3 @@
-// import { useState } from "react"
-
-// export const ItemCount = ({onAdd, stock}) => {
-//     const [count, setCount] = useState(1)
-
-//     const handleIncrease = () => {
-//         if(count < stock ) setCount((prev) => prev + 1);
-//     };
-
-//     const handleDecrease = () => {
-//         if(count > 1) setCount((prev) => prev - 1);
-//     };
-
-//     const handleAdd = () => {
-//         onAdd(count);
-//         setCount(1);
-//     }
-
-//     return (<>
-//     <button onClick={handleDecrease}>-</button>
-
-//     <span>{count}</span>
-//     <button onClick={handleIncrease}>+</button>
-
-//     <button onClick={handleAdd}>Comprar</button>
-
-//     </>);
-// };
-
 import { useState } from "react";
 import Button from 'react-bootstrap/Button';
 
